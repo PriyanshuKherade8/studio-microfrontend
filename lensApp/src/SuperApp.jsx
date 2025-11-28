74e0a2e8ebc3c43c2072470ceb3b1bdf5d5a8306
PriyanshuKherade8 <priyanshu.stark2@gmail.com>
2025-11-28 15:27:31 +0530

    event change

--- lensApp/src/SuperApp.jsx
+++ lensApp/src/SuperApp.jsx
@@ -2,22 +2,32 @@ import React, { Suspense, useEffect } from "react";
 const Canvas = React.lazy(() => import("canvas/App"));
 //const ControlsPanel = React.lazy(() => import('controlApp/ControlsPanel'));
 import eventBus from "core/eventBus";
-import { CORE, LENS } from "core/events";
+import { CORE, LENS, SHELL } from "core/events";
 import { useGetAllSets } from "./services/common";
 
 export default function SuperApp() {
-  // const { setObject } = useSet("micro")
-  const { data: allData } = useGetAllSets("micro");
+  const params = new URLSearchParams(window.location.search);
+  const value = params.get("set");
+
+  const { data: allData } = useGetAllSets(value);
   const setAllData = allData?.data;
-  console.log("insideSuperApp", allData);
 
   useEffect(() => {
-    eventBus.emit(CORE.IS_READY, "LENS");
-  }, []);
+    if (value) {
+      eventBus.emit(CORE.IS_READY, "LENS");
+    }
+  }, [value]);
 
   useEffect(() => {
+    if (!value) return;
+    // if (!setAllData) return;
+
+    eventBus.on(SHELL.PARAMS_INIT, (data) => {
+      console.log("data11", data);
+    });
+
     eventBus.emit(LENS.SET_INIT, { set: setAllData });
-  }, []);
+  }, [value, setAllData]);
 
   return (
     <div style={{ display: "flex", fontFamily: "Arial" }}>
